7ad7badd3e8718ae1372674930cbe388cd8e48d5
Dylan Scully <dylan08code@gmail.com>
2025-08-15 02:20:30 +0100

    feat: solution to problem 17
    Solution to problem 17 of project euler, Number Letter Counts.

--- Project Euler/main.js
+++ Project Euler/main.js
@@ -107,3 +107,75 @@ function digitSum(base, power) {
 
 	return sum;
 }
+
+// Problem 17
+// Sum of letters to represent numbers from 1 to 1000
+function letterSum() {
+	let characterSum = 0;
+	const digitToWord = {
+		1: 'one',
+		2: 'two',
+		3: 'three',
+		4: 'four',
+		5: 'five',
+		6: 'six',
+		7: 'seven',
+		8: 'eight',
+		9: 'nine',
+		10: 'ten',
+		11: 'eleven',
+		12: 'twelve',
+		13: 'thirteen',
+		14: 'fourteen',
+		15: 'fifteen',
+		16: 'sixteen',
+		17: 'seventeen',
+		18: 'eighteen',
+		19: 'nineteen',
+		20: 'twenty',
+		30: 'thirty',
+		40: 'forty',
+		50: 'fifty',
+		60: 'sixty',
+		70: 'seventy',
+		80: 'eighty',
+		90: 'ninety',
+	};
+
+	// Convert the number to words and add the length of the word variant
+	for (let i = 1; i < 1001; i++) {
+		let words = '';
+
+		if (i === 1000) {
+			words = 'onethousand';
+		} else {
+			// Hundreds column
+			const hundreds = Math.floor(i / 100);
+			if (hundreds > 0) {
+				words += digitToWord[hundreds] + 'hundred';
+			}
+
+			// Tens column including teens
+			const tens = Math.floor(i / 10) - hundreds * 10;
+			if (tens === 1) {
+				words += digitToWord[i % 100];
+			} else if (tens > 1) {
+				words += digitToWord[tens * 10];
+			}
+
+			// Ones column only if there is no teen
+			const ones = i % 10;
+			if (ones > 0 && tens !== 1) {
+				words += digitToWord[ones];
+			}
+
+			// Should and be added or not
+			if (hundreds > 0 && (tens > 0 || ones > 0) && i > 9) {
+				words += 'and';
+			}
+		}
+		characterSum += words.length;
+	}
+
+	return characterSum;
+}
